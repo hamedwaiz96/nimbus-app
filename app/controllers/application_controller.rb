4d36dbafaf8e3f9d1f7bf453f564e58c2aa6af92
Hamed Waiz <hamedw96@mgmail.com>
2019-08-23 21:53:46 -0700

    Initial Setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    helper_methods :current_user, :logged_in?
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(:session_token => session[:session_token])
+    end
+
+    def logged_in?
+        return !!current_user
+    end
+
+    def login(user)
+        user.reset_session_token!
+        session[:session_token] = user.session_token
+        @current_user = user
+    end 
+
+    def logout
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def require_logged_in
+        unless current_user
+            render json: ["Invalid Credentials"], status: 401
+        end
+    end
 end
